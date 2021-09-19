d0b56db23f4211ffa3bd87227fcc8dac855446f1
Gojou-Satoru-8 <ankushbhowmiktesting@gmail.com>
2021-09-19 12:05:31 +0530

    Redesigned Database

--- main.py
+++ main.py
@@ -22,39 +22,6 @@ db = SQLAlchemy(app)
 
 ##CONFIGURE TABLES
 
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
-    id = db.Column(db.Integer, primary_key=True)
-    # Linking the two two tables via 1:N relationshp between User to BlogPost:
-    # 1st step is specifying relationship (posts = relationship("BlogPost") line) in the parent table. posts will be a
-    # field that doesn't appear on the table. Rather, relationship() field/variable is only for specifying relationships
-
-    # 2nd step is creating a new column for the Foreign Key in the child table, here it's author_id which takes in
-    # users.id primary key value from User class ie. users table (the parent table here)
-    # Remember: inside db.ForeignKey(), the parameter is "__tablename__.<primary_key of parent table>"
-    # Thus, for the User class table with __tablename__ = "users", it is users.id not User.id
-
-    # 3rd step (bidirectional relationship) : use back_populates="relationship field name" inside relationship() field
-    # or variable inside both the parent and child class. As BlogPost class (child table) does not normally require a
-    # relationship() field (relationships are only specified on the parent), for this case, create a relationship()
-    # field inside the BlogPost, which back_populates to the relationship() field int User class, ie users table.
-    # Remember: inside relationship(), the parameters are : (1)Table Class Name, here, the value of __tablename__
-    # and (2) back-populates =<The relationship field name in the other table>
-
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)    # Step 2
-
-    # Following Columns were there from before
-    author = relationship("User", back_populates="posts")   # Step 3
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-    # 1:N relationship between BlogPost and Comment
-    comments = relationship("Comment", back_populates="parent_post")
-    
-# db.create_all()
-
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
@@ -62,23 +29,51 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    # Adding a 1:N relationship between User and BlogPost
     posts = relationship("BlogPost", back_populates="author")   # Step 1, also step 3 (back_populates)
+    # Adding a 1:N relationship between User and Comment
     comments = relationship("Comment", back_populates="commenter")
-# Note that whenever you access relationship() fields, say you query a BlogPost object, and tap into the author
-# property, like post1 = db.session.query(BlogPost).get(1), then author1 = post1.author, it will return the entire User
-# object. From there, you can access all properties of the User object, ie. author1.name (or id, password) et...
+    # Adding a N:N relationship between User and Category (Interests) remains
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    # Adding a author_id Foreign Key with bidirectional 1:N relationship between User and BlogPost
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)  # Step 2
+    author = relationship("User", back_populates="posts")  # Step 3
+    # Adding a category_id Foreign Key with bidirectional 1:N relationship between Category and BlogPost
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    category = relationship("Category", back_populates="posts")
+    # 1:N relationship between BlogPost and Comment
+    comments = relationship("Comment", back_populates="parent_post")
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    # Adding a 1:N relationship between Category and BlogPost
+    posts = relationship("BlogPost", back_populates="category")
 
 
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
-    # 1:N relationship between User to Comment
+    # Adding a commenter_id Foreign Key with bidirectional 1:N relationship between User and Comment
     commenter_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     commenter = relationship("User", back_populates="comments")
-    # 1:N relationship between BlogPost to Comment
+    # Adding a post_id Foreign Key with bidirectional 1:N relationship between BlogPost and Comment
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"), nullable=False)
     parent_post = relationship("BlogPost", back_populates="comments")
     text = db.Column(db.Text, nullable=False)   # Content of the comment
+
+# db.create_all()
 
 
 login_manager = LoginManager(app=app)
@@ -91,7 +86,7 @@ def admin_only(func):
             return func(*args, **kwargs)
         else:
             return abort(403)
-
+        
     # update_wrapper(decorated_function, func)    # Can be used instead of @wraps(func) above decorated function def
     return decorated_function
 
@@ -99,6 +94,7 @@ def admin_only(func):
 @app.errorhandler(403)
 def access_denied(error):       # This arg is required, probably some other function passes something, as error shows
     return render_template("error403.html"), 403    # access_denied takes 0 positional arg, but 1 one was given
+
 
 @app.errorhandler(404)
 def access_denied(error):
@@ -145,11 +141,7 @@ def login():
         if user_to_check:
             if check_password_hash(pwhash=user_to_check.password, password=login_form.password.data):
                 login_user(user_to_check)
-                # flash("Logged in successfully!")  # If flash messages aren't printed with Jinja in the right html
-                # file, they are stored in buffer, until user loads up a page where the flashed messages are
-                # caught with get_flashed_messages() function followed by: {{ message }}, which means that even if the
-                # page was supposed to deliver some other message/ no message (depending on return statements under that
-                # route, the original message will be delivered. Hence, please make sure of the order.
+
                 return redirect(url_for("get_all_posts"))
             else:
                 flash("Incorrect password entered! Please try again.")
