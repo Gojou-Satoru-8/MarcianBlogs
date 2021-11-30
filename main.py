41e44978e6f333d47060699708a18ac4c6ca7653
Gojou-Satoru-8 <ankushbhowmiktesting@gmail.com>
2021-12-01 05:07:57 +0530

    Final level changes made: (1) Reset Password Form page added. (2) All routes have been protected from URL exploitation with authenticity_protect decorator (3) The Error handlers work along with login_manager.unauthorized_handler (4) Changed phone number to StringField, and updated the database similarly. (5) Commented out the profile picture part of user_page.html and user-profile-contact.html pages. These will be added later (5) Miscellaneous other changed made, everything feels more robust
    Things that remain:
    (1) Forgot Password email resetting
    (2) Hosting
    (3) If possible in next update, we'll add profile picture uploads.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort
+from flask import Flask, render_template, redirect, url_for, flash, abort, request
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import datetime
@@ -26,9 +26,12 @@ Bootstrap(app)
 # # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:86IamAnkush@localhost:5432/blogs'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+photos = UploadSet("photos", IMAGES)
+app.config["UPLOADED_PHOTOS_DEST"] = "static/imgs/profile_pics"
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000
 
 
 # # CONFIGURE TABLES
@@ -44,7 +47,7 @@ class User(db.Model, UserMixin):
     pfp_name = db.Column(db.String(300))
     pfp_data = db.Column(db.LargeBinary)
     # Adding a 1:N relationship between User and BlogPost
-    posts = relationship("BlogPost", back_populates="author")   # Step 1, also step 3 (back_populates)
+    posts = relationship("BlogPost", back_populates="author")  # Step 1, also step 3 (back_populates)
     # Adding a 1:N relationship between User and Comment
     comments = relationship("Comment", back_populates="commenter")
     # Adding a N:N relationship between User and Category (Interests) remains
@@ -85,11 +88,13 @@ class Comment(db.Model):
     # Adding a post_id Foreign Key with bidirectional 1:N relationship between BlogPost and Comment
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"), nullable=False)
     parent_post = relationship("BlogPost", back_populates="comments")
-    text = db.Column(db.Text, nullable=False)   # Content of the comment
+    text = db.Column(db.Text, nullable=False)  # Content of the comment
     datetime = db.Column(db.DateTime, nullable=False)
 
 
 db.create_all()
+
+
 # all_categories = db.session.query(Category).order_by(Category.name).all()
 # print(all_categories)
 # all_categories_name = [category.name for category in all_categories]
@@ -106,14 +111,16 @@ class CreatePostForm(FlaskForm):
     all_categories_name = [category.name for category in all_categories]
     category = SelectField(u"Choose the category that best suits your Blog", validators=[DataRequired()],
                            choices=all_categories_name)
-    submit = SubmitField("Submit Post")
+    submit = SubmitField("Post Your Blog")
 
 
 class RegisterForm(FlaskForm):
     username = StringField(label="Username", validators=[DataRequired()])
-    email = StringField(label="Email", validators=[DataRequired(), Email(granular_message=True, check_deliverability=True)])
+    email = StringField(label="Email", validators=[DataRequired(),
+                                                   Email(granular_message=True, check_deliverability=True)])
     password = PasswordField(label="Password",
-                             validators=[DataRequired(), EqualTo(fieldname='password_check', message="Passwords must match")])
+                             validators=[DataRequired(),
+                                         EqualTo(fieldname='password_check', message="Passwords must match")])
     password_check = PasswordField(label="Confirm Password", validators=[DataRequired()])
     submit = SubmitField(label="Sign Me Up!")
 
@@ -137,7 +144,7 @@ class SearchForm(FlaskForm):
 
 class GetInTouchForm(FlaskForm):
     name = StringField(label="Name", validators=[DataRequired()])
-    phone = IntegerField(label="Phone", validators=[DataRequired()])
+    phone = StringField(label="Phone", validators=[DataRequired()])
     email = StringField(label="Email",
                         validators=[DataRequired(), Email(granular_message=True, check_deliverability=True)])
     message = TextAreaField(label="Message", validators=[DataRequired()])
@@ -154,23 +161,74 @@ class EditProfileForm(FlaskForm):
                     validators=[FileAllowed(upload_set=UploadSet('images', IMAGES), message="Images only!")])
     submit = SubmitField("Confirm Changes")
 
-# class ResetForm(FlaskForm):
-#     password = StringField()
-#     confirm_pass = StringField()
+
+class ResetForm(FlaskForm):
+    current_password = PasswordField(label="Current Password", validators=[DataRequired()])
+    new_password = PasswordField(label="New Password",
+                             validators=[DataRequired(),
+                                         EqualTo(fieldname="confirm_password", message="Passwords must match")])
+    confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired()])
+    submit = SubmitField(label="Reset Password")
 
 
 login_manager = LoginManager(app=app)
 
 
 def admin_only(func):
-    @wraps(func)                      # Or use update_wrapper function once the wrapper/decorated function is defined
+    @wraps(func)  # Or use update_wrapper function once the wrapper/decorated function is defined
     def decorated_function(*args, **kwargs):
         if current_user.id == 1 and current_user.is_authenticated:
             return func(*args, **kwargs)
         else:
             return abort(403)
-        
+
     # update_wrapper(decorated_function, func)    # Can be used instead of @wraps(func) above decorated function def
+    return decorated_function
+
+
+# def authenticity_protect(func):   # If you're directly using user_id
+#     @wraps(func)
+#     def decorated_function(user_id):
+#         if current_user == User.query.get(user_id):  # Basically checking if the current_user is the one
+#             return func(user_id)                     # accessing the protected webpage
+#         else:
+#             return abort(403)
+#     return decorated_function
+
+
+def authenticity_protect(func):  # If you're using *args and **kwargs as arguments in decorated_function
+    # This function will be used with editing pages (edit_post and edit_user_profile) and deleting routes,
+    # such as delete_post and delete_comment, as such the view functions will be decorated so that access can
+    # be forbidden in five cases:
+    # (1) Editing user profile: In this case, the user can be identified by the user_id keyword argument
+    # (2) Resetting password: Here too, the user can be identified by the user_id keyword argument
+    # (3) Editing a post: Here, the post_id will be used to query the post, whose author will be the appropriate user
+    # (4) Deleting a post: Here too, the post_id will be used to query the post, and post.author will be the user
+    # (5) Deleting a comment: This case will be covered last; the comment_id will be used to find the comment object,
+    # whose commenter will be the appropriate user to limit the access to.
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        # print(args, kwargs)   # It shows that the user_id gets passed in as a keyword argument, so
+        # to retrieve the user_id or post_id, use kwargs.get("user_id") or kwargs.get("post_id"), not args[0].
+
+        print(f"user_id: {kwargs.get('user_id')}, post_id: {kwargs.get('post_id')}, comment_id: {kwargs.get('comment_id')}")
+        user = User.query.get(kwargs.get("user_id"))  # For view functions which have user_id as argument
+        print(f"User(object): {user}, BlogPost(object): {BlogPost.query.get(kwargs.get('post_id'))}")
+
+        # If user is None, then either it was called from edit-post(post_id) function where there's no user_id arg,
+        # Or the user_id exceeds the max user_id, either way the following logic works:
+        if not user:
+            if BlogPost.query.get(kwargs.get("post_id")):
+                user = BlogPost.query.get(kwargs.get("post_id")).author
+            elif Comment.query.get(kwargs.get("comment_id")):   # Finally, if user and BlogPost object are both None
+                user = Comment.query.get(kwargs.get("comment_id")).commenter
+
+        if current_user == user:
+            # Basically checking if the current_user is the one accessing the protected webpage
+            return func(*args, **kwargs)
+        else:
+            return abort(403)
+
     return decorated_function
 
 
@@ -178,6 +236,25 @@ def admin_only(func):
 def load_user(user_id):
     print(f"The user logged in currently under login_manager.user_loader is {user_id}")
     return User.query.get(int(user_id))
+
+
+# This view function will be triggered, whenever there is an abort(404) or return abort(404) in other view functions
+@app.errorhandler(404)
+def page_no_exist(error):
+    return render_template("page-404.html", all_categories=Category.query.order_by(Category.name).all())
+
+
+# This is the error handler for 403 - Forbidden, whenever an explicit error is raised i.e abort(403)
+@app.errorhandler(403)
+def access_denied(error):  # This arg is required, probably some other function passes something, as error shows
+    return render_template("error403.html", all_categories=Category.query.order_by(
+        Category.name).all()), 403  # access_denied takes 0 positional arg, but 1 one was given
+
+
+# This is different from above as it is triggered by @login_required, whenever current_user is
+@login_manager.unauthorized_handler  # anonymous / not logged in
+def unauthorized_callback():
+    return render_template("error403.html", all_categories=Category.query.order_by(Category.name).all()), 403
 
 
 @app.route('/')
@@ -222,7 +299,8 @@ def register():
         login_user(new_user)
         return redirect(url_for("home"))
 
-    return render_template("register.html", form=register_form, all_categories=Category.query.order_by(Category.name).all())
+    return render_template("register.html", form=register_form,
+                           all_categories=Category.query.order_by(Category.name).all())
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -253,22 +331,25 @@ def login():
 def logout():
     logout_user()
     print(f"User ID: {current_user} logged out")
-    return redirect(url_for('home'))
+    flash("Logged out successfully")
+    return redirect(url_for('login'))
 
 
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
+    if not requested_post:
+        return abort(404)
     post_author_id = requested_post.author_id
     other_posts_by_same_author = BlogPost.query.filter_by(author_id=post_author_id).all()
-    other_posts_by_same_author.remove(requested_post)   # Removing the current post
+    other_posts_by_same_author.remove(requested_post)  # Removing the current post
     comment_form = CommentForm()
     if comment_form.validate_on_submit():
         if current_user.is_authenticated:
             new_comment = Comment(text=comment_form.comment.data, commenter=current_user,
                                   parent_post=requested_post, datetime=datetime.utcnow())
             db.session.add(new_comment)
-            db.session.commit()     # Following line is optional, as it would lead to the final return render_template
+            db.session.commit()  # Following line is optional, as it would lead to the final return render_template
             # return redirect(url_for("show_post", post_id=post_id))    # statement, to render the same page.
         else:
             flash("You need to login or register to comment")
@@ -294,7 +375,7 @@ def about_us():
                                 msg=f"Subject:Marcian Blogs - Get In Touch\n\n{mail_contents}")
         confirmation_msg = "Message Sent Successfully"
     else:
-        print("Form not validated")
+        print("Form not validated OR not a post request")
     return render_template("about-us.html", form=form, confirmation_msg=confirmation_msg,
                            all_categories=Category.query.order_by(Category.name).all())
 
@@ -305,7 +386,7 @@ def about_us():
 
 
 @app.route("/new-post", methods=["GET", "POST"])
-# @admin_only
+@login_required
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -326,9 +407,12 @@ def add_new_post():
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-# @admin_only
+@authenticity_protect
+@login_required
 def edit_post(post_id):
     post = db.session.query(BlogPost).get(post_id)
+    if not post:
+        return abort(404)
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
@@ -350,7 +434,8 @@ def edit_post(post_id):
 
 
 @app.route("/delete-post/<int:post_id>")
-# @admin_only
+@authenticity_protect
+@login_required
 def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
@@ -359,21 +444,13 @@ def delete_post(post_id):
 
 
 @app.route("/delete-comment/<int:comment_id>")
+@authenticity_protect
+@login_required
 def delete_comment(comment_id):
     comment_to_delete = Comment.query.get(comment_id)
     db.session.delete(comment_to_delete)
     db.session.commit()
-    return redirect(url_for("show_post", post_id=comment_to_delete.post_id ))
-
-
-@app.errorhandler(403)
-def access_denied(error):       # This arg is required, probably some other function passes something, as error shows
-    return render_template("error403.html"), 403    # access_denied takes 0 positional arg, but 1 one was given
-
-
-@app.errorhandler(404)
-def access_denied(error):
-    return render_template("page-404.html", all_categories=Category.query.order_by(Category.name).all())
+    return redirect(url_for("show_post", post_id=comment_to_delete.post_id))
 
 
 # For development purpose only
@@ -386,6 +463,8 @@ def access_denied(error):
 @app.route("/user/<int:user_id>")
 def posts_by_user(user_id):
     user = db.session.query(User).get(user_id)
+    if not user:
+        return abort(404)
     blogs_from_this_user = user.posts
     return render_template("user_page.html", user=user, user_posts=blogs_from_this_user,
                            all_categories=Category.query.order_by(Category.name).all())
@@ -394,6 +473,8 @@ def posts_by_user(user_id):
 @app.route("/user/<int:user_id>/profile", methods=["GET", "POST"])
 def show_user_profile(user_id):
     user = db.session.query(User).get(user_id)
+    if not user:
+        return abort(404)
     form = GetInTouchForm()
     confirmation_msg = None
     if form.validate_on_submit():
@@ -407,15 +488,22 @@ def show_user_profile(user_id):
 
             confirmation_msg = "Mail sent successfully"
     else:
-        print("Form not validated")
+        print("Form not validated OR not a post request")
 
     return render_template("user-profile-contact.html", user=user, form=form, confirmation_msg=confirmation_msg,
                            all_categories=Category.query.order_by(Category.name).all())
 
 
-@app.route("/user/<int:user_id>/profile/edit", methods=["GET", "POST"])
+@app.route("/user/<int:user_id>/edit-profile", methods=["GET", "POST"])
+@authenticity_protect
+@login_required
 def edit_user_profile(user_id):
+    # Following line are not required, as login_required takes care
+    # if not current_user.is_authenticated:   # Or if current_user.is_anonymous:
+    #     return abort(403)       # Or just abort(403)
     user = db.session.query(User).get(user_id)
+    if not user:
+        return abort(404)
     edit_profile_form = EditProfileForm(username=user.username, email=user.email,
                                         short_desc=user.short_desc, long_desc=user.long_desc)
     if edit_profile_form.validate_on_submit():
@@ -423,11 +511,43 @@ def edit_user_profile(user_id):
         user.email = edit_profile_form.email.data
         user.short_desc = edit_profile_form.short_desc.data
         user.long_desc = edit_profile_form.long_desc.data
+        file_uploaded = request.files
+        print(edit_profile_form.username.data, edit_profile_form.email.data, edit_profile_form.short_desc.data)
+        print(file_uploaded)
+        # print(edit_profile_form.pfp.object_data)
         db.session.commit()
-        return redirect(url_for("show_user_profile", user_id=user.id))      # Or user_id=user_id
+        return redirect(url_for("show_user_profile", user_id=user.id))  # Or user_id=user_id
 
     return render_template("edit-user-profile.html", user=user, form=edit_profile_form,
                            all_categories=Category.query.order_by(Category.name).all())
+
+
+@app.route("/user/<int:user_id>/password-reset", methods=["GET", "POST"])
+@authenticity_protect
+@login_required
+def reset_password(user_id):
+    user = db.session.query(User).get(user_id)
+    form = ResetForm()
+    if form.validate_on_submit():
+        if check_password_hash(pwhash=user.password, password=form.current_password.data):
+            if check_password_hash(pwhash=user.password, password=form.new_password.data):
+                flash("New password cannot be identical to old password")
+            else:
+                user.password = generate_password_hash(password=form.new_password.data, method="pbkdf2:sha256", salt_length=8)
+                db.session.commit()
+                logout_user()
+                mail_contents = "Your password was reset. If this was not you, please contact us at " \
+                                "ankushbhowmiktesting@gmail.com"
+                with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+                    connection.starttls()
+                    connection.login(user="ankushbhowmiktesting@gmail.com", password="Ankush123*()")
+                    connection.sendmail(from_addr="ankushbhowmiktesting@gmail.com", to_addrs=user.email,
+                                        msg=f"Subject:Marcian Blogs - Password Reset\n\n{mail_contents}")
+                flash(message="Password changed successfully. Please log in again!")
+                return redirect(url_for("login"))
+        else:
+            flash(message="Current Password is Wrong!")
+    return render_template("password_reset.html", form=form, user=user)
 
 
 @app.route("/category/<int:cat_id>")
