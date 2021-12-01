64ea22f55c57f03e37bb35019974657864c8eb82
Gojou-Satoru-8 <ankushbhowmiktesting@gmail.com>
2021-12-02 01:36:05 +0530

    (1) changed hard-coded secret_key and database_uri to the environment ones Things that remain: (1) Forgot Password email resetting (3) If possible in next update, we'll add profile picture uploads.

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ from flask_ckeditor import CKEditorField
 from functools import wraps, update_wrapper
 import smtplib
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:86IamAnkush@localhost:5432/blogs'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://postgres:86IamAnkush@localhost:5432/blogs'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
